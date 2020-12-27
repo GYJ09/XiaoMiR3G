832e1744c12e4f58ba6fdd4d0f268c3df2144f2d
GYJ09 <76415815+GYJ09@users.noreply.github.com>
2020-12-27 10:05:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,11 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+
+# 自定义插件
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
